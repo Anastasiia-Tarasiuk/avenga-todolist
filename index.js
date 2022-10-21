3a386016ed5149ec6c561edf607375829c9aaa13
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-10-21 16:47:18 +0300

    Adds items counter

--- index.js
+++ index.js
@@ -23,19 +23,18 @@ incompletedTodosCounterEl.textContent = 0;
     
 function onButtonClick() {
     const dataIndex = Math.random().toString(36).slice(-6);
-    createIncompletedTodoItem(dataIndex)
+    renderTodoItem(dataIndex, inputEl.value, incompletedTodoListEl)
     setArrayItem(inputEl.value, dataIndex)
     inputEl.value = "";
-    // updateTodoCounter();
 }
 
-function createIncompletedTodoItem(dataIndex) {
-    incompletedTodoListEl.insertAdjacentHTML('afterbegin', todoItemTemplate);
-    const todoItemTextEl = incompletedTodoListEl.querySelector('.todoItemText');
-    todoItemTextEl.textContent = inputEl.value;
-    const todoItemEl = incompletedTodoListEl.querySelector('.todoItem');
-    todoItemEl.setAttribute('data-index', dataIndex);
-    
+function renderTodoItem(dataIndex, text, list) {
+    list.insertAdjacentHTML('afterbegin', todoItemTemplate);
+    const todoItemTextEl = list.querySelector('.todoItemText');
+    todoItemTextEl.textContent = text;
+    const todoItemEl = list.querySelector('.todoItem');
+    todoItemEl.setAttribute('data-index', dataIndex);  
+    updateTodoCounter();
 }
 
 function setArrayItem(value, dataIndex) {
@@ -48,110 +47,53 @@ function setArrayItem(value, dataIndex) {
     todoItemArray.push(item);
 }
 
-
-// function updateTodoCounter() {
-//     if (completedTodoListEl.children.length > 0) {
-//        completedTodosCounterEl.textContent = completedTodoListEl.children.length;
-//     } else {
-//         completedTodosCounterEl.textContent = 0;
-//     }
-
-//     if (incompletedTodoListEl.children.length > 0) {
-//         incompletedTodosCounterEl.textContent = incompletedTodoListEl.children.length;
-//     } else {
-//         completedTodosCounterEl.textContent = 0;
-//     }
-// }
+function updateTodoCounter() {
+    completedTodosCounterEl.textContent = completedTodoListEl.children.length;
+    incompletedTodosCounterEl.textContent = incompletedTodoListEl.children.length;
+}
 
 function onCheckBoxClick(e) {
     const itemDataIndex = e.parentElement.getAttribute('data-index');
+    const todoTextContent = e.parentElement.querySelector('.todoItemText').textContent;
 
     if (e.checked) {
-        const incompletedTodoItemEls = incompletedTodoListEl.querySelectorAll('.todoItem');
+        changeCheckboxValue(itemDataIndex, incompletedTodoListEl, 'true');
+        renderTodoItem(itemDataIndex, todoTextContent, completedTodoListEl);
 
-        changeCheckboxValue([...incompletedTodoItemEls], itemDataIndex, incompletedTodoListEl, 'true');
+        const checkboxEl = completedTodoListEl.querySelector('.completeItemButton');
+        checkboxEl.setAttribute('checked', '');
         
-        renderCompletedItems(e.parentElement);
     } else {
-        const completedTodoItemEls = completedTodoListEl.querySelectorAll('.todoItem');
-
-        changeCheckboxValue([...completedTodoItemEls], itemDataIndex, completedTodoListEl, 'false');
-        renderIncompletedItems(e.parentElement);
+        changeCheckboxValue(itemDataIndex, completedTodoListEl, 'false');
+        renderTodoItem(itemDataIndex, todoTextContent, incompletedTodoListEl);
+        
+        const checkboxEl = incompletedTodoListEl.querySelector('.completeItemButton');
+        checkboxEl.removeAttribute('checked');
     }
 }
 
-function changeCheckboxValue(array, index, element, dataDoneStatus) {
-    array.map(el => {
+function changeCheckboxValue(index, element, dataDoneStatus) {
+    [...element.querySelectorAll('.todoItem')].map(el => {
             if (el.getAttribute('data-index') === index) {
                 el.setAttribute('data-done-status', dataDoneStatus);
-                
                 removeFormDom(index, element);
             }
-        })
-
+    })
+    updateTodoCounter();
 }
 
 function onRemoveBtnClick(e) {
     const itemDataIndex = e.parentElement.getAttribute('data-index');
 
-    // remove item from array
     removeItem(itemDataIndex);
-   
-    // remove item from dom in every list
+    
     const allTodoItemEls = document.querySelectorAll('.todoItem');
     [...allTodoItemEls].map(el => {
         if (el.getAttribute('data-index') === itemDataIndex) {
             removeFormDom(itemDataIndex, document);
         }
     });
-}
-
-function itemActions(e) {
-//     const element = e.target;
-//     const item = element.closest('li');
-//     const checkbox = element.closest('input');
-//     const itemDataIndex = item.getAttribute('data-index');
-//
-//     const currentListClassName = element.parentElement.parentElement.className;
-//     let currentArray = null;
-//     let elementToDoAction = null;
-//
-//     if (currentListClassName === 'todoList') {
-//         currentArray = todoItemArray;
-//         elementToDoAction = todoListEl;
-//     }
-//
-//     if (currentListClassName === 'completedTodoList') {
-//         currentArray = todoCompletedItemArray;
-//         elementToDoAction = completedTodoListEl;
-//     }
-//
-//     if (currentListClassName === 'incompletedTodoList') {
-//         currentArray = todoIncompletedItemArray;
-//         elementToDoAction = incompletedTodoListEl;
-//     }
-//
-//     // ------------
-//
-//     if (element.className === "removeItemButton") {
-//         removeItem(itemDataIndex, currentArray, elementToDoAction);
-//     }
-//
-//     if (element.className === "completeItemButton") {
-//         if (checkbox.checked) {
-//             todoItemArray.map(el => {
-//
-//                 if (el.id === item.getAttribute('data-index')) {
-//                     el.isDone = true;
-//                 }
-//             })
-//             setItemCompleted(item);
-//             removeItem(itemDataIndex, todoIncompletedItemArray, incompletedTodoListEl);
-//         } else {
-//             unsetItemCompleted(checkbox);
-//             removeItem(itemDataIndex, todoCompletedItemArray, completedTodoListEl);
-//         }
-//     }
+    updateTodoCounter();
 }
 
 function removeItem(id) {
@@ -159,40 +101,10 @@ function removeItem(id) {
         return item.id !== id;
     });
 
-    // updateTodoCounter();
 }
-
-
-// має бути один метод рендеру!
-function renderCompletedItems(element) {
-
-    completedTodoListEl.insertAdjacentHTML('afterbegin', todoItemTemplate);
-
-    const todoItemEl = completedTodoListEl.querySelector('.todoItem');
-    const todoItemTextEl = completedTodoListEl.querySelector('.todoItemText');
-    const completeItemButtonEl = completedTodoListEl.querySelector('.completeItemButton');
-    todoItemEl.setAttribute('data-index', element.getAttribute('data-index'));
-    todoItemTextEl.textContent = element.querySelector('p').textContent;
-    completeItemButtonEl.setAttribute('checked', '');
-
-    // updateTodoCounter();
-}
-
-function renderIncompletedItems(element) {
-    incompletedTodoListEl.insertAdjacentHTML('afterbegin', todoItemTemplate);
-
-    const todoItemEl = incompletedTodoListEl.querySelector('.todoItem');
-    const todoItemTextEl = incompletedTodoListEl.querySelector('.todoItemText');
-    const incompleteItemButtonEl = incompletedTodoListEl.querySelector('.completeItemButton');
-    todoItemEl.setAttribute('data-index', element.getAttribute('data-index'));
-    todoItemTextEl.textContent = element.querySelector('p').textContent;
-    incompleteItemButtonEl.removeAttribute('checked');
-
-    // updateTodoCounter();
-}
-
 
 function removeFormDom(id, element) {
     const item = element.querySelector(`[data-index="${id}"]`);
     item.remove();
 }
+
